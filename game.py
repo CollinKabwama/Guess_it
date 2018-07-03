cbf7fcff0fb2d9aab5c5feb4887fbcfeb1e4f12a
Kabwama <kabwamacollin291@gmail.com>
2018-07-03 17:11:03 +0300

    Lireally the best console game madegit commit -m

--- game.py
+++ game.py
@@ -15,7 +15,8 @@ def check_number():
                 print("Close")
                 correct_number_wrong_position = 1
                 break
-    if (correct_number_wrong_position != 1) and (correct_number_correct_position != 1):  #execute if both the above have failed
+    if (correct_number_wrong_position != 1) and (
+            correct_number_correct_position != 1):  # execute if both the above have failed
         print("Nope")
 
 
@@ -23,11 +24,34 @@ digits = list(range(10))
 random.shuffle(digits)
 number = digits[:3]
 
-while True:
-    guess = input("What is your guess? ")
-    guess = list(guess)  # co
-    num = map(lambda x: int(x), guess)  # hepls convert the list of strings into a list of integers
-    guessed_num = list(num)
-    check_number()
-    if guess == number:
-        break
+
+def game():
+    global guessed_num
+    while True:
+        try:
+            guess = input("Please enter a 3-digit guess: ")
+            if len(guess) > 3:
+                print("You entered more than 3 characters")
+                continue
+            guess = list(guess)  # convert string into a list
+            num = map(lambda x: int(x), guess)  # helps convert the list of strings into a list of integers
+            guessed_num = list(num)
+        except ValueError:
+            print("Please enter a number")
+            continue
+
+        check_number()
+        if guess == number:
+            print("You got it !!!\n The number ")
+            break
+    play_again = input("Wud you like to play again? Y/N")
+    if play_again.upper() == Y:
+        print("Game loading........")
+        game()
+    elif play_again.upper() == N:
+        print("Thanks for playing.\nGood bye")
+    else:
+        print("Invalid Input!!!!")
+
+
+game()
